3350fac8b8559c59d50e4d94db8ab7273e9e36e7
DG-Develop <dgdband@hotmail.com>
2020-04-18 14:47:35 -0500

    Finalizando la UI de fragment_data_dialog y el de fragment_data_subred_dialog

---
+++ app/src/main/java/com/david/redcristianauno/ui/fragments/DataFragment.kt
@@ -0,0 +1,23 @@
+package com.david.redcristianauno.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.david.redcristianauno.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DataFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_data, container, false)
+    }
+
+}
--- app/src/main/java/com/david/redcristianauno/ui/fragments/DataSubredDialogFragment.kt
+++ app/src/main/java/com/david/redcristianauno/ui/fragments/DataSubredDialogFragment.kt
@@ -1,23 +1,24 @@
-package com.david.redcristianauno.ui
+package com.david.redcristianauno.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.david.redcristianauno.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class DataFragment : Fragment() {
+class DataSubredDialogFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data, container, false)
+        return inflater.inflate(R.layout.fragment_data_subred_dialog, container, false)
     }
 
 }
